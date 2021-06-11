8f4a13757edad16bfe978e0793d00d00d7e9b644
karsay <ryu47seinou@gmail.com>
2021-06-11 14:01:52 +0900

    最初のコミット

--- app/src/main/java/nhs90556/hal/ac/u_22/ui/home/HomeFragment.kt
+++ app/src/main/java/nhs90556/hal/ac/u_22/ui/home/HomeFragment.kt
@@ -1,31 +1,89 @@
 package nhs90556.hal.ac.u_22.ui.home
 
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import nhs90556.hal.ac.u_22.R
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private var resultText = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getWeather("nagoya-shi")
+        Thread.sleep(1000)
+
+        weatherText.text = resultText
+    }
+
+    // unixtimeからフォーマットの日付に変換
+    private fun unixTimeChange(unixTime: String): String {
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm")
+        val nowTime = Date(unixTime.toInt() * 1000L)
+        return sdf.format(nowTime)
+    }
+
+    // 天候データの取得
+    private fun getWeather(city: String):Job = GlobalScope.launch {
+
+        // APIを使う際に必要なKEY
+        val API_KEY = "6e8e86e4e208ed9480deb401bd28ba65"
+        // URL。場所と言語・API_KEYを添付
+        val API_URL = "https://api.openweathermap.org/data/2.5/forecast?" +
+                "q=" + city + ",jp&" +
+                "lang=" + "ja" + "&" +
+                "APPID=" + API_KEY
+        val url = URL(API_URL)
+
+        //APIから情報を取得する.
+        val br = BufferedReader(InputStreamReader(url.openStream()))
+        // 所得した情報を文字列化
+        val str = br.readText()
+        //json形式のデータとして識別
+        val json = JSONObject(str)
+        // 配列を取得
+        val list = json.getJSONArray("list")
+
+        //時間帯？
+        val Object = list.getJSONObject(0)
+
+        // 日時
+        val date = Object.getString("dt")
+        // 天候
+        val weather = Object.getJSONArray("weather").getJSONObject(0).getString("description")
+        // 気温
+        val temperature = Object.getJSONObject("main").getString("temp")
+        // 降水量
+        val rainfall = "0"
+
+        val descriptionText = "日時:" + unixTimeChange(date) + "\n天候:" + weather + "\n気温:" + temperature + "\n降水率" + rainfall
+
+        resultText = descriptionText
+
+    }
+
 }
