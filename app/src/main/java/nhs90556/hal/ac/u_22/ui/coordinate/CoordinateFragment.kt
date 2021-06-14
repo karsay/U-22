e35d72ba4c415c72d7ea7fd789e7f0ec60ff4f5a
YashiN1910 <yashintools@gmail.com>
2021-06-14 12:19:00 +0900

    フラグメント間での天候と服装指数の受け渡し

--- app/src/main/java/nhs90556/hal/ac/u_22/ui/coordinate/CoordinateFragment.kt
+++ app/src/main/java/nhs90556/hal/ac/u_22/ui/coordinate/CoordinateFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import kotlinx.android.synthetic.main.fragment_coordinate.*
 import nhs90556.hal.ac.u_22.R
 
 /**
@@ -21,5 +23,20 @@ class CoordinateFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_coordinate, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // 設定した requestKey を元にbundleを受け取る.服装指数と天候情報の受け取り.
+        setFragmentResultListener("weather_info") { requestKey, bundle ->
+            val weather = bundle.getString("result_weather")             // 天候
+            val clothesIndex = bundle.getInt("result_clothes_index")   // 服装指数
+
+            // 値の受け渡し確認のテスト
+            weatherInfoText.text = weather
+            clothesIndexText.text = clothesIndex.toString()
+        }
+
+    }
 
 }
